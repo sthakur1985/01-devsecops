d504c2d27f7a91097e7c381788dbe01e524aa2e5
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-05-26 11:18:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region = ${{ vars.AWS_REGION }}
-  access_key = ${{ vars.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ vars.AWS_SECRET_ACCESS_KEY }}
+
 }
 
 resource "aws_vpc" "http-vpc" {
