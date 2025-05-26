803f62c9c2d0c8b83b3cf235ec50990ab6d3e5bf
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-05-26 10:52:18 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "{{ vars.AWS_REGION }}"
+}
+
+resource "aws_vpc" "http-vpc" {
+  cidr_block            = "10.5.0.0/16"
+  enable_dns_support    = "true"
+  enable_dns_hostnames  = "true"
+  tags                  = {
+    name = "http-vpc"
+  }
+}
