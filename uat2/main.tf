37eb0a1869282fd364ddec4d3ffbf690b656f5b1
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-05-26 18:33:56 +0100

    Create main.tf

---
+++ uat2/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+
+}
+
+resource "aws_vpc" "http-vpc" {
+  cidr_block            = "10.5.0.0/16"
+  enable_dns_support    = "true"
+  enable_dns_hostnames  = "true"
+  tags                  = {
+    name = "http-vpc"
+  }
