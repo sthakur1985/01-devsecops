38f4658a4af3f3957b2a6bc781a877894f01888b
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-05-28 22:02:46 +0100

    Update backend.tf

--- uat1/backend.tf
+++ uat1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "terraform-bucket"
+     bucket = "terraform-bucket-st"
      key = "terraform.tfstate"
      region = "us-east-1"
      dynamodb_table = "tfstate-table"
